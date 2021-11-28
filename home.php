b43c3a859ff7fd889d01c3aa812f03332caa2e53
Joel Tafilaj <tafilaj82@gmail.com>
2021-11-28 23:14:38 +0100

    Website File Structure update

--- home.php
+++ home.php
@@ -14,17 +14,26 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
 
     <!-- Font Awesome Icons -->
-    <link href="https://use.fontawesome.com/6eb17e02f3.css" media="all" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" media="all" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Italiana&display=swap" rel="stylesheet">
+
+    <!-- CSS -->
+    <link href="inc/css/home.css" rel="stylesheet">
+    <link href="inc/css/navbar.css" rel="stylesheet">
+    <link href="inc/css/waves.css" rel="stylesheet">
 
 </head>
 <body>
-    
+    <!-- Navbar -->
+    <?php require 'inc/php/navbar.php'; ?>
 
 
     <!-- Script -->
 
         <!-- Font Awesome -->
-        <script src="https://use.fontawesome.com/2e0710846e.js" type="text/javascript"></script>    
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/js/all.min.js" type="text/javascript"></script>    
         
         <!-- Bootstrap -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
@@ -35,5 +44,9 @@
         
         <!-- AOS -->
         <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+        <!-- JS -->
+        <script src="inc/js/navbar.js"></script>
+        <script src="inc/js/waves.js"></script>
 </body>
 </html>
