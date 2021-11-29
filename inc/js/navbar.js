e5777318b991f8ef6afa206d09f2b3641e18be47
Enxhi Tabaku <enxhi.tabaku@gmail.com>
2021-11-29 22:37:06 +0100

    Home Page Template Test Version

--- inc/js/navbar.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $('#searchToggler').click(function (e) { 
-        e.preventDefault();
-        $('.form .form-control').removeClass('w-25');
-        $('.form').removeClass('d-none');
-        $('.form .form-control').addClass('w-100');
-        $('.form').addClass('d-flex');
-    });
-});
