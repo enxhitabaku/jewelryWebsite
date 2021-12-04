a67a608aac46f436ccdfac0240ab3455297e09d1
Joel Tafilaj <tafilaj82@gmail.com>
2021-12-05 00:51:42 +0100

    Single Product Page carousel section done

--- assets/js/singleproduct.js
+++ assets/js/singleproduct.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
-    $('.owl-carousel').owlCarousel({
-        loop:true,
+    $('.product-carousel').owlCarousel({
+        loop:false,
         margin:10,
         responsiveClass:true,
+        nav:false,
+        dots:true,
+        navText: ['<i class="fas fa-angle-left"></i>', '<i class="fas fa-angle-right"></i>'],
+        animateOut: 'fadeOut',
         responsive:{
             0:{
                 items:1,
