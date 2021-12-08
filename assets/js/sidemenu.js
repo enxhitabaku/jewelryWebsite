8ab51019d9efa42f271ca2a787fedd01527a74ae
Enxhi Tabaku <enxhi.tabaku@gmail.com>
2021-12-08 01:35:01 +0100

    Basic Side Navbar Created

---
+++ assets/js/sidemenu.js
@@ -0,0 +1,39 @@
+// ArrayIcon
+function togleArrow(el){
+    let currentArrowIcon = el.querySelector("i");
+    const defaultIconClass = currentArrowIcon.classList.contains("down"); 
+
+    if(defaultIconClass){
+        currentArrowIcon.classList.remove("down");
+        currentArrowIcon.classList.add("up");
+    }else{
+        currentArrowIcon.classList.remove("up");
+        currentArrowIcon.classList.add("down");
+    }
+
+}
+
+// Price
+function negateNegativePrice(el){
+    if(el.value < 0){
+        alert('Price Can Not Be Negative')
+        el.value = 0;
+    }
+       
+}
+
+// document.getElementById("minPrice").addEventListener('focusout',function(){
+//     const max = document.getElementById("maxPrice").value
+//     const min = document.getElementById("minPrice").value
+
+//     if(min > max)
+//         alert("Max Price shall be greater than Min Price");
+// });
+
+// document.getElementById("maxPrice").addEventListener('focusout',function(){
+//     const max = document.getElementById("maxPrice").value
+//     const min = document.getElementById("minPrice").value
+
+//     if(min > max)
+//         alert("Max Price shall be greater than Min Price");
+// });
