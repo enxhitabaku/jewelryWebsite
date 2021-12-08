f93d943c3338d5afff0f611c157b1f4722b93e2b
Joel Tafilaj <tafilaj82@gmail.com>
2021-12-08 18:43:45 +0100

    Registration Section created

--- assets/js/account.js
+++ assets/js/account.js
@@ -15,9 +15,11 @@ $(document).ready(function () {
         if(!$('.eye').hasClass('fa-eye-slash')){
             $('.eye').removeClass('fa-eye');
             $('.eye').addClass('fa-eye-slash');
+            $('#password').attr('type', 'text');
         } else {
             $('.eye').removeClass('fa-eye-slash');
             $('.eye').addClass('fa-eye');
+            $('#password').attr('type', 'password');
         }
     });
 });
