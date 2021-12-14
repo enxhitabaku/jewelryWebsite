0caf3e87eef9520106aa4560f77c09ed5452fcf3
Joel Tafilaj <tafilaj82@gmail.com>
2021-12-14 13:34:41 +0100

    Floating added

--- assets/js/account.js
+++ assets/js/account.js
@@ -114,4 +114,21 @@ $(document).ready(function () {
             $('#registerResponse').html('<i class="fas fa-check-circle"></i> Thank you for registering');
         }
     });
+
+
+
+
+
+
+    $(".field-wrapper .field-placeholder").on("click", function () {
+        $(this).closest(".field-wrapper").find("input").focus();
+    });
+    $(".field-wrapper input").on("keyup", function () {
+        var value = $.trim($(this).val());
+        if (value) {
+            $(this).closest(".field-wrapper").addClass("hasValue");
+        } else {
+            $(this).closest(".field-wrapper").removeClass("hasValue");
+        }
+    });
 });
